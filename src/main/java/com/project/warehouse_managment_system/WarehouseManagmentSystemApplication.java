6cfca241225233f8437e12e7a0c7be3c97b89a07
Amer <thelordamer2000@gmail.com>
2024-10-20 17:01:14 +0300

    first commit

---
+++ src/main/java/com/project/warehouse_managment_system/WarehouseManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.warehouse_managment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/warehouse_managment_system/WarehouseManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.warehouse_managment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
